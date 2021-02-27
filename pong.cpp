75729f5b5fd9e65e13fcf51a0a36286eed952b29
Benjamin Kelley <22280156+MWPuppire@users.noreply.github.com>
2021-02-26 21:00:55 -0500

    Fixed Pong clone

--- pong.cpp
+++ pong.cpp
@@ -13,27 +13,39 @@ double ballMove;
 double ballSpeed;
 int playerScore, computerScore;
 int lastScore;
-int playerControl;
+int ballCollisions;
+const double radian = M_PI / 180.0;
+const double velocityTransfer = 0.5;
+const double ballCollisionSpeed = 1.03;
 
 void initBall()
 {
 	const int width  = Astrum::window::getWidth();
 	const int height = Astrum::window::getHeight();
-	ballAngle = (lastScore ? 195 : 15) + Astrum::math::randfloat(60.0);
+	ballAngle = (lastScore ? 195 : 15) + Astrum::math::random(4) * 15.0;
 	ballX = (double) width / 2.0;
 	ballY = (double) height / 2.0;
 	ballMove = 0.0;
 	ballSpeed = width;
+	ballCollisions = 0;
+}
+
+void deflectBall(double xMod, double yMod, double xAdd, double yAdd)
+{
+	double ballVx = std::cos(ballAngle * radian) * ballSpeed * xMod + xAdd;
+	double ballVy = std::sin(ballAngle * radian) * ballSpeed * yMod + yAdd;
+	ballAngle = std::atan2(ballVy, ballVx) / radian;
+	while (ballAngle < 0)
+		ballAngle += 360.0;
+	while (ballAngle >= 360.0)
+		ballAngle -= 360.0;
+	ballSpeed = Astrum::window::getWidth() * std::pow(ballCollisionSpeed, ++ballCollisions);
 }
 
 void update(double dt)
 {
 	if (Astrum::keyboard::isdown("escape"))
 		Astrum::quit();
-
-	// turbo
-	if (Astrum::keyboard::isdown("t"))
-		dt *= 5;
 
 	const int width  = Astrum::window::getWidth();
 	const int height = Astrum::window::getHeight();
@@ -43,67 +55,60 @@ void update(double dt)
 	const double shapeWidth  = (double) width / 32.0;
 	const double speed = (double) height;
 
-	ballMove += dt;
+	if (ballMove < 0.5)
+		ballMove += dt;
 
-	if (playerControl) {
-		if (Astrum::keyboard::isdown("down") || Astrum::keyboard::isdown("s"))
-			player += speed * dt;
-		if (Astrum::keyboard::isdown("up") || Astrum::keyboard::isdown("w"))
-			player -= speed * dt;
-	} else {
-		if (player - shapeHeight / 2.0 < ballY)
-			player += speed * dt;
-		else if (player - shapeHeight / 2.0 > ballY)
-			player -= speed * dt;
+	double playerDir = 0.0;
+	if (Astrum::keyboard::isdown("down") || Astrum::keyboard::isdown("s")) {
+		player += speed * dt;
+		playerDir += 270.0;
+	}
+	if (Astrum::keyboard::isdown("up") || Astrum::keyboard::isdown("w")) {
+		player -= speed * dt;
+		playerDir += 90.0;
 	}
 	player = (player < shapeHeight ? shapeHeight : (player > height ? height : player));
-	if (computer - shapeHeight / 2.0 < ballY)
+	double compDir = 0.0;
+	if (computer - shapeHeight / 2.0 < ballY) {
 		computer += speed * dt;
-	else if (computer - shapeHeight / 2.0 > ballY)
+		compDir += 270.0;
+	} else if (computer - shapeHeight / 2.0 > ballY) {
 		computer -= speed * dt;
+		compDir += 90.0;
+	}
 	computer = (computer < shapeHeight ? shapeHeight : (computer > height ? height : computer));
 
-	const double radian = M_PI / 180.0;
 	if (ballMove >= 0.5) {
 		ballX += std::cos(ballAngle * radian) * ballSpeed * dt;
 		ballY += std::sin(ballAngle * radian) * ballSpeed * dt;
 	}
 	double playerDiff = Astrum::math::max(ballY - player, player - ballY);
-	if (ballX <= (shapeWidth * 2) && playerDiff <= shapeHeight && ballY <= player) {
-		ballAngle = ballAngle + 180.0 + Astrum::math::random(30.0);
-		ballSpeed *= 1.03;
+	if (ballX < shapeWidth && playerDiff <= shapeHeight && ballY <= player) {
+		double playerVel = speed * std::cos(playerDir * radian);
+		deflectBall(-1, 1, 0, velocityTransfer * playerVel);
+		ballX = shapeWidth;
 	} else if (ballX < halfSize) {
 		computerScore++;
 		lastScore = 0;
 		initBall();
 	}
 	double computerDiff = Astrum::math::max(ballY - computer, computer - ballY);
-	if (ballX >= (width - shapeWidth * 2) && computerDiff <= shapeHeight && ballY <= computer) {
-		ballAngle = ballAngle + 180.0 + Astrum::math::random(30.0);
-		ballSpeed *= 1.03;
+	if (ballX > (width - shapeWidth) && computerDiff <= shapeHeight && ballY <= computer) {
+		double computerVel = speed * std::cos(compDir * radian);
+		deflectBall(-1, 1, 0, velocityTransfer * computerVel);
+		ballX = width - shapeWidth;
 	} else if (ballX > width - halfSize) {
 		playerScore++;
 		lastScore = 1;
 		initBall();
 	}
 	if (ballY < halfSize) {
-		ballY = halfSize;
-		ballAngle = ballAngle + 180.0 + Astrum::math::random(30.0);
+		ballY = halfSize + 1;
+		deflectBall(1, -1, 0, 0);
 	} else if (ballY > height - halfSize) {
-		ballY = height - halfSize;
-		ballAngle = ballAngle + 180.0 + Astrum::math::random(30.0);
+		ballY = height - halfSize - 1;
+		deflectBall(1, -1, 0, 0);
 	}
-	while (ballAngle >= 360.0)
-		ballAngle -= 360.0;
-
-	std::ostringstream scorestream;
-	scorestream << "Player: " << playerScore << " - " << "Computer: " << computerScore;
-	std::string str = scorestream.str();
-	const char *toprint = str.c_str();
-	int textWidth, textHeight;
-	Astrum::Font *font = Astrum::graphics::getFont();
-	font->textSize(toprint, &textWidth, &textHeight);
-	Astrum::graphics::print(toprint, (width - textWidth) / 2, 5, white);
 }
 
 void draw()
@@ -114,15 +119,18 @@ void draw()
 	const double shapeWidth  = (double) width / 32.0;
 	const double circleSize  = (double) width / 24.0;
 
-	Astrum::graphics::rectangle(shapeWidth, player - shapeHeight, shapeWidth, shapeHeight, white, true);
-	Astrum::graphics::rectangle(width - 2 * shapeWidth, computer - shapeHeight, shapeWidth, shapeHeight, white, true);
+	Astrum::graphics::rectangle(0, player - shapeHeight, shapeWidth, shapeHeight, white, true);
+	Astrum::graphics::rectangle(width - shapeWidth, computer - shapeHeight, shapeWidth, shapeHeight, white, true);
 	Astrum::graphics::circle(ballX, ballY, circleSize, white, true);
-}
 
-void keypressed(Astrum::Keycode key)
-{
-	if (key == SDLK_RETURN)
-		playerControl = playerControl ? 0 : 1;
+	std::ostringstream scorestream;
+	scorestream << "Player: " << playerScore << " - " << "Computer: " << computerScore;
+	std::string str = scorestream.str();
+	const char *toprint = str.c_str();
+	int textWidth, textHeight;
+	Astrum::Font *font = Astrum::graphics::getFont();
+	font->textSize(toprint, &textWidth, &textHeight);
+	Astrum::graphics::print(toprint, (width - textWidth) / 2, 5, white);
 }
 
 void startup()
@@ -136,7 +144,6 @@ void startup()
 	player = computer = center;
 	playerScore = computerScore = 0;
 	lastScore = 0;
-	playerControl = 1;
 	initBall();
 }
 
@@ -144,12 +151,13 @@ int main()
 {
 	Astrum::Config conf;
 	conf.windowFullscreen = false;
+	conf.windowResizable = true;
+	conf.windowTitle = "Pong Test";
 
 	Astrum::Init(&conf);
 
 	Astrum::onstartup(startup);
 	Astrum::ondraw(draw);
-	Astrum::onkeypressed(keypressed);
 
 	Astrum::run(update);
 
